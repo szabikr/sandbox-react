dc3c571658428104a1043fb53147a9a9d4243aa8
szabikr <szabikr.dev@gmail.com>
2022-01-11 19:11:41 +0000

    boilerplate: initial webpack dev server setup

---
+++ boilerplate/src/app.js
@@ -0,0 +1,6 @@
+import { Component } from './component'
+
+window.addEventListener('load', () => {
+  console.log('window loaded')
+  Component()
+})
---
+++ boilerplate/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 8080,
+    hot: true,
+    open: true,
+  },
+};
