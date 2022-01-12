4d595d9f5d34e4870e9abe8ca26b093d071ca7c5
szabikr <szabikr.dev@gmail.com>
2022-01-12 19:09:34 +0000

    blog-post-w-comments: page skeleton

--- blog-post-w-comments/src/app.jsx
+++ blog-post-w-comments/src/app.jsx
@@ -1,5 +1,41 @@
 import React from 'react'
 
 export default function App() {
-  return <h1>Blog Post with Comments</h1>
+  return (
+    <>
+      <h1>Blog Post with Comments</h1>
+      <p>2022 January</p>
+      <div>
+        Blog post content
+      </div>
+      <div>
+        <p>Comments</p>
+        <div>
+          <ul>
+            <li>
+              Name: John Smith <br />
+              Content: This is a comment <br />
+              Date: 2022 Jan
+            </li>
+            <li>
+              Name: Jane Doe <br />
+              Content: This is another comment <br />
+              Date: 2022 Jan
+            </li>
+          </ul>
+          <div>
+            <div>
+              <label for="name-field">Name</label>
+              <input id="name-field" type="text" />
+            </div>
+            <div>
+              <label for="content-field">Comment</label>
+              <textarea id="content-field" />
+            </div>
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
