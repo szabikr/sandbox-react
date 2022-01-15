0d54dab5c3c790a1d1bef4a15aacdd7867a2fda1
szabikr <szabikr.dev@gmail.com>
2022-01-15 17:32:19 +0000

    boilerplate: additional styling

--- boilerplate/src/app.jsx
+++ boilerplate/src/app.jsx
@@ -4,7 +4,8 @@ import styles from './styles/app.module.css'
 export default function App() {
   return (
     <div className={styles.container}>
-      <h1>Szabi Space React Boilerplate</h1>
+      <h1>React Boilerplate</h1>
+      <div class={styles.subtitle}>a <strong>szabi.space</strong> development</div>
     </div>
   )
 }
