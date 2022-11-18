8d6df91ebe353b40a70aa360f8e6a93d2672bf86
szabikr <szabikr.dev@gmail.com>
2022-11-18 10:23:31 +0200

    Implements and designs todo list web app that is saved in local storage

---
+++ todo-items-local-storage/src/App.js
@@ -0,0 +1,110 @@
+import { useState, useEffect, useRef } from 'react'
+import { v4 as uuid } from 'uuid'
+import './App.css'
+
+const constantTodos = [
+  {
+    id: uuid(),
+    name: 'shopping',
+    complete: true,
+  },
+  {
+    id: uuid(),
+    name: 'workout',
+    complete: false,
+  },
+  {
+    id: uuid(),
+    name: 'coding',
+    complete: false,
+  },
+]
+
+const TODO_ITEMS_KEY = 'todoItems'
+
+function App() {
+  const todoItemInputRef = useRef(null)
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = JSON.parse(localStorage.getItem(TODO_ITEMS_KEY))
+    return savedTodos || constantTodos
+  })
+
+  useEffect(() => {
+    localStorage.setItem(TODO_ITEMS_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodoClick() {
+    const name = todoItemInputRef.current.value
+    if (name === '') {
+      return
+    }
+    setTodos([
+      ...todos,
+      {
+        id: uuid(),
+        name,
+        complete: false,
+      },
+    ])
+    todoItemInputRef.current.value = ''
+  }
+
+  function handleClearClick() {
+    setTodos(todos.filter((todo) => !todo.complete))
+  }
+
+  function handleToggleTodoChange(id) {
+    setTodos([
+      ...todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete,
+          }
+        }
+        return todo
+      }),
+    ])
+  }
+
+  return (
+    <>
+      <main>
+        <h1>Todo List</h1>
+        <section>
+          <label htmlFor="new-todo-item">Would like to accomplish</label>
+          <input id="new-todo-item" ref={todoItemInputRef} type="text" />
+          <button class="primary" onClick={handleAddTodoClick}>
+            Add todo
+          </button>
+          <button class="secondary" onClick={handleClearClick}>
+            Clear list
+          </button>
+        </section>
+
+        <section>
+          {todos.map((todo) => (
+            <div key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.complete}
+                onChange={() => handleToggleTodoChange(todo.id)}
+              />
+              <span>{todo.name}</span>
+            </div>
+          ))}
+        </section>
+      </main>
+      <hr />
+      <footer>
+        a{' '}
+        <strong>
+          <a href="https://szabi.space/">szabi.space</a>
+        </strong>{' '}
+        development
+      </footer>
+    </>
+  )
+}
+
+export default App
