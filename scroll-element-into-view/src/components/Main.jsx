705c527e62d2d52e2bc24987c477ca019e7ce081
szabikr <szabikr.dev@gmail.com>
2023-04-16 15:25:22 +0300

    Removes unused code

--- scroll-element-into-view/src/components/Main.jsx
+++ scroll-element-into-view/src/components/Main.jsx
@@ -50,26 +50,3 @@ const Main = forwardRef(function Main(props, ref) {
 });
 
 export default Main;
-
-function Main1({ expertiseSectionRef, blogSectionRef, contactSectionRef }) {
-  return (
-    <main>
-      <section>
-        <h1>Scroll Element into View</h1>
-        <p>Click on the menu items to scroll to a section.</p>
-      </section>
-      <section id="expertise" ref={expertiseSectionRef}>
-        <h1>Expertise</h1>
-        <p>Experienced Software Egnineer.</p>
-      </section>
-      <section id="blog" ref={blogSectionRef}>
-        <h1>Blog</h1>
-        <p>Blog posts are coming soon...</p>
-      </section>
-      <section id="contact" ref={contactSectionRef}>
-        <h1>Contact Me</h1>
-        <p>Send me an e-mail and let's talk about the details.</p>
-      </section>
-    </main>
-  );
-}
