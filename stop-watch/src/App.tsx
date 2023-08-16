59eac925ea82204e2de26d8ed7b890faf0962fd2
szabikr <szabikr.dev@gmail.com>
2023-08-16 12:39:59 +0300

    Implements start stop and reset timer

--- stop-watch/src/App.tsx
+++ stop-watch/src/App.tsx
@@ -1,10 +1,54 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function getSeconds(milis: number): number {
+  return Math.floor(milis / 1000)
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [startTime, setStartTime] = useState<number | null>(null)
+  const [recentlyElapsedTime, setRecentlyElapsedTime] = useState<number>(0)
+  const [elapsedTime, setElapsedTime] = useState<number>(0)
+  const [lapTimes, setLapTimes] = useState<number[]>([])
+
+  useEffect(() => {
+    let intervalId: number | undefined = undefined
+
+    if (startTime === null) {
+      if (intervalId) {
+        window.clearInterval(intervalId)
+      }
+
+      return
+    }
+
+    intervalId = window.setInterval(() => {
+      setRecentlyElapsedTime(Date.now() - startTime)
+    }, 1000)
+
+    return () => window.clearInterval(intervalId)
+  }, [startTime])
+
+  function startTimer() {
+    setStartTime(Date.now())
+  }
+
+  function stopTimer() {
+    setElapsedTime(elapsedTime + recentlyElapsedTime)
+    setRecentlyElapsedTime(0)
+    setStartTime(null)
+  }
+
+  function lapTimer() {
+    const newLapTimes = [...lapTimes, elapsedTime + recentlyElapsedTime]
+    setLapTimes(newLapTimes)
+  }
+
+  function resetTimer() {
+    setElapsedTime(0)
+  }
 
   return (
     <>
@@ -16,11 +60,23 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Stopwatch</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div>
+          {startTime ? (
+            <button onClick={() => stopTimer()} style={{ marginLeft: '0.5em' }}>
+              Stop
+            </button>
+          ) : (
+            <button onClick={() => startTimer()}>Start</button>
+          )}
+          {startTime ? (
+            <button onClick={() => lapTimer()}>Lap</button>
+          ) : (
+            <button onClick={() => resetTimer()}>Reset</button>
+          )}
+        </div>
+        <p>Elapsed Time: {getSeconds(elapsedTime + recentlyElapsedTime)}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
