92420d1a64457a153266c4f2c7479da79dbccfbd
Szabi Keresztes <szabikr.dev@gmail.com>
2023-08-23 11:39:35 +0300

    Stopwatch: Refactor - Extracting logic Timer logic from components (#8)
    * Extracts timer functionality and implements sample UI
    
    * Updates App and other components to make use of the timer module
    
    * Removes unnecessary line

--- stop-watch/src/App.tsx
+++ stop-watch/src/App.tsx
@@ -1,96 +1,54 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import useTimer from './hooks/timer'
+
 import MainTimer from './components/MainTimer'
 import Controls from './components/Controls'
 import Laps from './components/Laps'
 
-function App() {
-  const [startTime, setStartTime] = useState<number | null>(null)
-  const [recentlyElapsedTime, setRecentlyElapsedTime] = useState<number>(0)
-  const [elapsedTime, setElapsedTime] = useState<number>(0)
-
-  const [startLapTime, setStartLapTime] = useState<number | null>(null)
-  const [recentlyElapsedLapTime, setRecentlyElapsedLapTime] =
-    useState<number>(0)
-  const [elapsedLapTime, setElapsedLapTime] = useState<number>(0)
+export default function AppUseTimer() {
+  const mainTimer = useTimer()
+  const lapTimer = useTimer()
 
   const [lapTimes, setLapTimes] = useState<number[]>([])
 
-  // === Global Timer ===
-  useEffect(() => {
-    if (startTime === null) return
-
-    const intervalId = window.setInterval(() => {
-      setRecentlyElapsedTime(Date.now() - startTime)
-    }, 10)
-
-    return () => window.clearInterval(intervalId)
-  }, [startTime])
-
-  // === Current Lap Timer ===
-  useEffect(() => {
-    if (startLapTime === null) return
-
-    const intervalId = window.setInterval(() => {
-      setRecentlyElapsedLapTime(Date.now() - startLapTime)
-    }, 10)
-
-    return () => window.clearInterval(intervalId)
-  }, [startLapTime])
-
-  function startTimer() {
+  function start() {
     const currentTime = Date.now()
-    setStartTime(currentTime)
-    setStartLapTime(currentTime)
+    mainTimer.start(currentTime)
+    lapTimer.start(currentTime)
   }
 
-  function stopTimer() {
-    setElapsedTime(elapsedTime + recentlyElapsedTime)
-    setRecentlyElapsedTime(0)
-    setStartTime(null)
-
-    setElapsedLapTime(elapsedLapTime + recentlyElapsedLapTime)
-    setRecentlyElapsedLapTime(0)
-    setStartLapTime(null)
+  function stop() {
+    mainTimer.stop()
+    lapTimer.stop()
   }
 
-  function lapTimer() {
-    setLapTimes([...lapTimes, elapsedLapTime + recentlyElapsedLapTime])
-
-    setElapsedLapTime(0)
-    setRecentlyElapsedLapTime(0)
-    setStartLapTime(Date.now())
-  }
-
-  function resetTimer() {
-    setElapsedTime(0)
-    setElapsedLapTime(0)
+  function reset() {
+    mainTimer.reset()
+    lapTimer.reset()
     setLapTimes([])
+  }
+
+  function lap() {
+    setLapTimes([...lapTimes, lapTimer.elapsedTime])
+    lapTimer.reset()
+    lapTimer.start(Date.now())
   }
 
   return (
     <>
       <h1>Stopwatch</h1>
-      <MainTimer
-        elapsedTime={elapsedTime}
-        recentlyElapsedTime={recentlyElapsedTime}
-      />
+      <MainTimer elapsedTime={mainTimer.elapsedTime} />
       <Controls
-        startTime={startTime}
-        startTimer={startTimer}
-        stopTimer={stopTimer}
-        lapTimer={lapTimer}
-        resetTimer={resetTimer}
+        isTimerRunning={mainTimer.isRunning}
+        start={start}
+        stop={stop}
+        reset={reset}
+        lap={lap}
       />
-      <Laps
-        lapTimes={lapTimes}
-        elapsedLapTime={elapsedLapTime}
-        recentlyElapsedLapTime={recentlyElapsedLapTime}
-      />
+      <Laps lapTimes={lapTimes} elapsedLapTime={lapTimer.elapsedTime} />
       <footer>
         a <a href="https://szabi.space">szabi.space</a> development
       </footer>
     </>
   )
 }
-
-export default App
--- stop-watch/src/components/MainTimer.tsx
+++ stop-watch/src/components/MainTimer.tsx
@@ -3,16 +3,8 @@ import './MainTimer.css'
 
 export interface MainTimerProps {
   elapsedTime: number
-  recentlyElapsedTime: number
 }
 
-export default function MainTimer({
-  elapsedTime,
-  recentlyElapsedTime,
-}: MainTimerProps) {
-  return (
-    <p className="time elapsed-time">
-      {getSeconds(elapsedTime + recentlyElapsedTime)}
-    </p>
-  )
+export default function MainTimer({ elapsedTime }: MainTimerProps) {
+  return <p className="time elapsed-time">{getSeconds(elapsedTime)}</p>
 }
