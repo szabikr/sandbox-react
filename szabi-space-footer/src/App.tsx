6534786ef54d675bec93a34f3385dbe4acde8d97
Szabi Keresztes <szabikr.dev@gmail.com>
2023-09-08 13:09:29 +0300

    Start Szabi Space Footer work (#11)

---
+++ szabi-space-footer/src/App.tsx
@@ -0,0 +1,16 @@
+import SzabiSpaceFooter from './components/SzabiSpaceFooter'
+import Content from './components/Content'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <SzabiSpaceFooter />
+    </>
+  )
+}
+
+export default App
---
+++ szabi-space-footer/src/components/SzabiSpaceFooter.tsx
@@ -0,0 +1,7 @@
+export default function SzabiSpaceFooter() {
+  return (
+    <footer>
+      a <a href="https://szabi.space">szabi.space</a> development
+    </footer>
+  )
+}
