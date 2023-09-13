fcb905159906590274d0a456f67bc8ea5cafc971
szabikr <szabikr.dev@gmail.com>
2023-09-13 20:43:38 +0300

    Align countdown to center

--- countdown/src/components/Main.tsx
+++ countdown/src/components/Main.tsx
@@ -10,9 +10,12 @@ export default function Main({ eventName, eventDate }: MainProps) {
 
   return (
     <main>
-      <h1>{eventName} in</h1>
+      <h1>{eventName}</h1>
       <h2>
         <span>{remaining.days} days</span>
+        {
+          // Pad these values with 0 when they are single digit
+        }
         <span>{remaining.hours} hours</span>
         <span>{remaining.minutes} minutes</span>
         <span>{remaining.seconds} seconds</span>
